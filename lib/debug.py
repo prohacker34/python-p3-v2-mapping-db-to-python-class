f7aad6d3e2358708149cfee15eeb861a09e36cfa
Habert <habertotieno4@gmail.com>
2025-05-26 16:20:15 +0300

    database row to python

--- lib/debug.py
+++ lib/debug.py
@@ -7,6 +7,7 @@ import ipdb
 def reset_database():
     Department.drop_table()
     Department.create_table()
+    
 
     Department.create("Payroll", "Building A, 5th Floor")
     Department.create("Human Resources", "Building C, East Wing")
